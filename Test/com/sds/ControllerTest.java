d3292bc38a40d82db7aa82162bb50b2db906ab7a
Steven Ballard <stevenballardstudios@gmail.com>
2020-03-04 14:25:39 -0800

    Added a blank controller test that fails

---
+++ Test/com/sds/ControllerTest.java
@@ -0,0 +1,14 @@
+package com.sds;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ControllerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
