6cf942f5c2a8e32959e09a59bf08e6e3df0f3609
148426 <148426@G919LLAX.ustr.com>
2020-03-05 11:51:09 -0800

    added constructor for DBHandler

--- src/com/sds/DBHandler.java
+++ src/com/sds/DBHandler.java
@@ -1,5 +1,28 @@
 package com.sds;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class DBHandler {
+	
+	private static final String GET_ALL_CONTRACTS = "select * from contracts";
+	
+	private static final String URL = "jdbc:mysql://localhost:3306/safedataspace";
+	private static final String USER = "root";
+	private static final String PASS = "root";
+	
+	private Connection con;
+	private ResultSet rs = null;
+	private PreparedStatement pst = null;
+	
+	public DBHandler() throws SQLException {
+		this.con = DriverManager.getConnection(URL,USER,PASS);
+	}
+	
+	
+	
 
 }
